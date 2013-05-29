a01961eebacb828c7372d0a949480dcb24acf45d
Gino Paulaitis <g.paulaitis@gmail.com>
2013-05-30 00:07:30 +0200

    Implementing harmonic features.

--- src/main/java/ch/hsr/mixtape/features/harmonic/HarmonicFeaturesExtractor.java
+++ src/main/java/ch/hsr/mixtape/features/harmonic/HarmonicFeaturesExtractor.java
@@ -2,6 +2,8 @@ package ch.hsr.mixtape.features.harmonic;
 
 import java.util.List;
 
+import com.google.common.primitives.Ints;
+
 import ch.hsr.mixtape.features.FeatureExtractor;
 import ch.hsr.mixtape.processing.MathUtils;
 
@@ -10,28 +12,59 @@ public class HarmonicFeaturesExtractor implements FeatureExtractor<HarmonicFeatu
 	private static final int WINDOW_SIZE = 4096;
 	private static final int WINDOW_OVERLAP = 2048;
 
-	private FundamentalFrequencies fundamentals;
-	private Harmonics harmonics;
+	private FundamentalFrequencies fundamentals = new FundamentalFrequencies(WINDOW_SIZE);
+	private Harmonics harmonics = new Harmonics();
 
-	private Inharmonicity inharmonicity;
-	private OddToEvenHarmonicEnergyRatio oddToEvenHarmonicEnergyRatio;
+	private Inharmonicity inharmonicity = new Inharmonicity();
+	private OddToEvenHarmonicEnergyRatio oddToEvenHarmonicEnergyRatio = new OddToEvenHarmonicEnergyRatio();
 
 	public HarmonicFeaturesOfWindow extractFrom(double[] window) {
 		double[] frequencySpectrum = MathUtils.fft(window);
 		double[] powerSpectrum = MathUtils.multiply(frequencySpectrum, frequencySpectrum);
 
-		HarmonicFeaturesOfWindow harmony = new HarmonicFeaturesOfWindow();
+		HarmonicFeaturesOfWindow featuresOfWindow = new HarmonicFeaturesOfWindow();
 
-		harmony.fundamentals = fundamentals.extract(frequencySpectrum);
-		harmony.harmonics = harmonics.extract(powerSpectrum, harmony.fundamentals);
-		harmony.inharmonicity = inharmonicity.extract(powerSpectrum, harmony.fundamentals, harmony.harmonics);
-		harmony.oddToEvenHarmonicEnergyRatio = oddToEvenHarmonicEnergyRatio.extract(powerSpectrum, harmony.harmonics);
+		featuresOfWindow.fundamentals = fundamentals.extract(frequencySpectrum);
+		featuresOfWindow.harmonics = harmonics.extract(powerSpectrum, featuresOfWindow.fundamentals);
+		featuresOfWindow.inharmonicity = inharmonicity.extract(powerSpectrum, featuresOfWindow.fundamentals,
+				featuresOfWindow.harmonics);
+		featuresOfWindow.oddToEvenHarmonicEnergyRatio = oddToEvenHarmonicEnergyRatio.extract(powerSpectrum,
+				featuresOfWindow.harmonics);
 
-		return harmony;
+		return featuresOfWindow;
 	}
 
-	public HarmonicFeaturesOfSong postprocess(List<HarmonicFeaturesOfWindow> x) {
-		return null;
+	public HarmonicFeaturesOfSong postprocess(List<HarmonicFeaturesOfWindow> featuresOfWindows) {
+		HarmonicFeaturesOfSong featuresOfSong = new HarmonicFeaturesOfSong();
+
+		int[] fundamentals = new int[0];
+		int[] harmonics = new int[0];
+		int[] inharmonicity = new int[featuresOfWindows.size()];
+		int[] oddToEvenHarmonicEnergyRatio = new int[featuresOfWindows.size()];
+
+		for (int i = 0; i < featuresOfWindows.size(); i++) {
+			HarmonicFeaturesOfWindow featuresOfWindow = featuresOfWindows.get(i);
+
+			fundamentals = Ints.concat(fundamentals, featuresOfWindow.fundamentals);
+			harmonics = Ints.concat(harmonics, featuresOfWindow.harmonics);
+			inharmonicity[i] = quantizeInharmonicity(featuresOfWindow.inharmonicity);
+			oddToEvenHarmonicEnergyRatio[i] = quantizeOddToEvenHarmonicEnergyRatio(featuresOfWindow.oddToEvenHarmonicEnergyRatio);
+		}
+
+		featuresOfSong.fudamentals = fundamentals;
+		featuresOfSong.harmonics = harmonics;
+		featuresOfSong.inharmonicity = inharmonicity;
+		featuresOfSong.oddToEvenHarmonicEnergyRatio = oddToEvenHarmonicEnergyRatio;
+
+		return featuresOfSong;
+	}
+
+	private int quantizeInharmonicity(double inharmonicity) {
+		return 0;
+	}
+
+	private int quantizeOddToEvenHarmonicEnergyRatio(double oddToEvenHarmonicEnergyRatio) {
+		return 0;
 	}
 
 	public double distanceBetween(HarmonicFeaturesOfSong x, HarmonicFeaturesOfSong y) {
--- src/main/java/ch/hsr/mixtape/features/harmonic/HarmonicFeaturesOfSong.java
+++ src/main/java/ch/hsr/mixtape/features/harmonic/HarmonicFeaturesOfSong.java
@@ -2,4 +2,9 @@ package ch.hsr.mixtape.features.harmonic;
 
 public class HarmonicFeaturesOfSong {
 
+	public int[] fudamentals;
+	public int[] harmonics;
+	public int[] inharmonicity;
+	public int[] oddToEvenHarmonicEnergyRatio;
+
 }
