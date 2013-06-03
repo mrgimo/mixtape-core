216ed1bae04b720b9a54b6a9ce541c114279b836
Gino Paulaitis <g.paulaitis@gmail.com>
2013-06-03 16:33:12 +0200

    Added new method to limit values.

--- src/main/java/ch/hsr/mixtape/MathUtils.java
+++ src/main/java/ch/hsr/mixtape/MathUtils.java
@@ -108,4 +108,13 @@ public class MathUtils {
 		return argMax;
 	}
 
+	public static double limit(double value, double min, double max) {
+		if (value < min)
+			return min;
+		else if (value > max)
+			return max;
+		else
+			return value;
+	}
+
 }
